f98e3660801078875a26e9e59e0907f94b91373a
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-01-27 19:18:51 +0530

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -30,13 +30,17 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concept</h2>
           <ul>
-            <Coreconcept
+            {/* <Coreconcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image}
             />
             <Coreconcept {...CORE_CONCEPTS[1]} />
-            <Coreconcept {...CORE_CONCEPTS[2]} />
+            <Coreconcept {...CORE_CONCEPTS[2]} /> */}
+            {/* better way to output list dynamically by using map */}
+            {CORE_CONCEPTS.map((conceptItems) => (
+              <Coreconcept key={conceptItems.title} {...conceptItems} />
+            ))}
           </ul>
         </section>
         <section id="examples">
